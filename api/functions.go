0cd89c804585684b2b2a8bef926e1bcdbc9a124a
Xeckt <jordansavell1198@gmail.com>
2025-06-02 12:59:08 +0100

    add all api function constants

--- api/functions.go
+++ api/functions.go
@@ -1,5 +1,49 @@
 package api
 
 const apiHealthCheckFunction = "HealthCheck"
+
 const VerifyAuthTokenFunction = "VerifyAuthenticationToken"
+
+const PasswordlessLoginFunction = "PasswordlessLogin"
+
+const PasswordLoginFunction = "PasswordLogin"
+
+const QueryServerStateFunction = "QueryServerState"
+
 const GetServerOptionsFunction = "GetServerOptions"
+
+const GetAdvancedGameSettingsFunction = "GetAdvancedGameSettings"
+
+const ApplyAdvancedGameSettingsFunction = "ApplyAdvancedGameSettings"
+
+const ClaimServerFunction = "ClaimServer"
+
+const RenameServerFunction = "RenameServer"
+
+const SetClientPasswordFunction = "SetClientPassword"
+
+const SetAdminPasswordFunction = "SetAdminPassword"
+
+const SetAutoLoadSessionNameFunction = "SetAutoLoadSessionName"
+
+const RunCommandFunction = "RunCommand"
+
+const ShutdownFunction = "Shutdown"
+
+const ApplyServerOptionsFunction = "ApplyServerOptions"
+
+const CreateNewGameFunction = "CreateNewGame"
+
+const SaveGameFunction = "SaveGame"
+
+const DeleteSaveFileFunction = "DeleteSaveFile"
+
+const DeleteSaveSessionFunction = "DeleteSaveSession"
+
+const EnumerateSessionsFunction = "EnumerateSessions"
+
+const LoadGameFunction = "LoadGame"
+
+const UploadSaveGameFunction = "UploadSaveGame"
+
+const DownloadSaveGameFunction = "DownloadSaveGame"
