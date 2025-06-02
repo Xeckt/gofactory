2d2af7a164ca30f462f2b2b1dae74690a3ffa67d
Xeckt <jordansavell1198@gmail.com>
2025-06-02 15:32:21 +0100

    add advanced settings

---
+++ api/advanced-settings.go
@@ -0,0 +1,38 @@
+package api
+
+type AppliedAdvancedGameSettings struct {
+	NoPower                         string `json:"FG.GameRules.NoPower"`
+	StartingTier                    string `json:"FG.GameRules.StartingTier"`
+	DisableArachnidCreatures        string `json:"FG.GameRules.DisableArachnidCreatures"`
+	NoUnlockCost                    string `json:"FG.GameRules.NoUnlockCost"`
+	SetGamePhase                    string `json:"FG.GameRules.SetGamePhase"`
+	UnlockAllResearchSchematics     string `json:"FG.GameRules.UnlockAllResearchSchematics"`
+	UnlockInstantAltRecipes         string `json:"FG.GameRules.UnlockInstantAltRecipes"`
+	UnlockAllResourceSinkSchematics string `json:"FG.GameRules.UnlockAllResourceSinkSchematics"`
+	NoBuildCost                     string `json:"FG.PlayerRules.NoBuildCost"`
+	GodMode                         string `json:"FG.PlayerRules.GodMode"`
+	FlightMode                      string `json:"FG.PlayerRules.FlightMode"`
+}
+
+type advancedGameSettingsResponse struct {
+	Data struct {
+		Settings AppliedAdvancedGameSettings `json:"AppliedAdvancedGameSettings"`
+	} `json:"data"`
+}
+
+func (c *GoFactoryClient) GetAdvancedGameSettings() (*AppliedAdvancedGameSettings, *APIError, error) {
+	request, err := c.createPostRequest(GetAdvancedGameSettingsFunction, createGenericFunctionBody(GetAdvancedGameSettingsFunction))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var query advancedGameSettingsResponse
+	apiErr, err := c.sendPostRequest(request, &query)
+	if err != nil {
+		return nil, nil, err
+	}
+	if apiErr != nil {
+		return nil, apiErr, nil
+	}
+	return &query.Data.Settings, nil, nil
+}
