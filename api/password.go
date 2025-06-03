154cf619d2159c7176090af2b47653425579d118
Xeckt <jordansavell1198@gmail.com>
2025-06-03 09:49:45 +0100

    rename passwordless-login.go to password.go and add pasword login support

---
+++ api/password.go
@@ -0,0 +1,79 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+)
+
+const NOT_AUTHENTICATED_PRIVILEGE string = "NotAuthenticated"
+const CLIENT_PRIVILEGE string = "ClientPrivilege"
+const ADMINISTRATOR_PRIVILEGE string = "Administrator"
+const INITIAL_ADMIN_PRIVILEGE string = "InitialAdmin"
+const API_TOKEN_PRIVILEGE string = "ApiToken"
+
+type PasswordlessLoginRequest struct {
+	Function string                       `json:"function,omitempty"`
+	Data     PasswordlessLoginRequestData `json:"data,omitempty"`
+}
+
+type PasswordLoginRequest struct {
+	Function string                   `json:"function,omitempty"`
+	Data     PasswordLoginRequestData `json:"data,omitempty"`
+}
+
+type PasswordLoginRequestData struct {
+	MinimumPrivilegeLevel string `json:"minimumPrivilegeLevel,omitempty"`
+	Password              string `json:"password,omitempty"`
+}
+
+type PasswordlessLoginRequestData struct {
+	MinimumPrivilegeLevel string `json:"minimumPrivilegeLevel,omitempty"`
+}
+
+type LoginResponse struct {
+	Data PasswordLoginResponseData `json:"data,omitempty"`
+}
+
+type PasswordLoginResponseData struct {
+	AuthToken string `json:"authenticationToken,omitempty"`
+}
+
+func (c *GoFactoryClient) PasswordlessLogin(ctx context.Context, privilege string) (*PasswordLoginResponseData, error) {
+	functionBody, err := json.Marshal(PasswordlessLoginRequest{
+		Function: PasswordlessLoginFunction,
+		Data: PasswordlessLoginRequestData{
+			MinimumPrivilegeLevel: privilege,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	tokenResponse, err := CreateAndSendPostRequest[LoginResponse](ctx, c, PasswordlessLoginFunction, functionBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenResponse.Data, nil
+}
+
+func (c *GoFactoryClient) PasswordLogin(ctx context.Context, privilege string, password string) (*PasswordLoginResponseData, error) {
+	functionBody, err := json.Marshal(PasswordLoginRequest{
+		Function: PasswordLoginFunction,
+		Data: PasswordLoginRequestData{
+			MinimumPrivilegeLevel: privilege,
+			Password:              password,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	tokenResponse, err := CreateAndSendPostRequest[LoginResponse](ctx, c, PasswordLoginFunction, functionBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenResponse.Data, nil
+}
