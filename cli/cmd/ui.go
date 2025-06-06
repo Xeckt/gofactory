7738a4e98a604923927d8c65d80fbb7713d7d30b
Xeckt <jordansavell1198@gmail.com>
2025-06-06 19:38:34 +0100

    fix: add ui.go

---
+++ cli/cmd/ui.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/alchemicalkube/gofactory/api"
+	"github.com/pterm/pterm"
+)
+
+var (
+	selectMenu = pterm.InteractiveSelectPrinter{
+		TextStyle:     pterm.NewStyle(pterm.FgLightCyan),
+		DefaultText:   "select a command to run",
+		Options:       []string{"query server", "health check", "login"},
+		OptionStyle:   pterm.NewStyle(pterm.FgCyan),
+		DefaultOption: "",
+		MaxHeight:     5,
+		Selector:      "➜",
+		SelectorStyle: pterm.NewStyle(pterm.FgCyan),
+		Filter:        true,
+	}
+
+	loginMenu = pterm.InteractiveSelectPrinter{
+		TextStyle:     pterm.NewStyle(pterm.FgLightCyan),
+		DefaultText:   "select the type of login to try",
+		Options:       []string{"password", "passwordless"},
+		OptionStyle:   pterm.NewStyle(pterm.FgCyan),
+		DefaultOption: "",
+		MaxHeight:     5,
+		Selector:      "➜",
+		SelectorStyle: pterm.NewStyle(pterm.FgCyan),
+		Filter:        true,
+	}
+
+	privilegeMenu = pterm.InteractiveSelectPrinter{
+		TextStyle:   pterm.NewStyle(pterm.FgLightCyan),
+		DefaultText: "select a privilege",
+		Options: []string{
+			api.NOT_AUTHENTICATED_PRIVILEGE,
+			api.INITIAL_ADMIN_PRIVILEGE,
+			api.CLIENT_PRIVILEGE,
+			api.CLIENT_PRIVILEGE,
+		},
+		OptionStyle:   pterm.NewStyle(pterm.FgCyan),
+		DefaultOption: "",
+		MaxHeight:     5,
+		Selector:      "➜",
+		SelectorStyle: pterm.NewStyle(pterm.FgCyan),
+		Filter:        true,
+	}
+)
