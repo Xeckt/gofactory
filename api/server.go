3cad0ad6af6bb3d1f3cb6f04fa8d72731dcd516c
Node <jordansavell1198@gmail.com>
2025-06-12 15:28:38 +0100

    move rename server into server.go for api

--- api/server.go
+++ api/server.go
@@ -86,7 +86,6 @@ func (c *GoFactoryClient) ApplyServerOptions(ctx context.Context, options Server
 // GetServerOptions retrieves the current and pending server options
 // from the Satisfactory dedicated server.
 func (c *GoFactoryClient) GetServerOptions(ctx context.Context) (*GetServerOptionsData, error) {
-
 	optionsResponse, err := CreateAndSendPostRequest[GetServerOptionsResponse](ctx, c,
 		GetServerOptionsFunction,
 		CreateGenericFunctionBody(GetServerOptionsFunction))
@@ -240,6 +239,47 @@ func (c *GoFactoryClient) ShutdownServer(ctx context.Context) error {
 	}
 
 	err = c.SendPostRequest(ctx, request, functionBody)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RenameRequest represents a request to rename the Satisfactory server.
+type RenameRequest struct {
+	// Function specifies the API function to call for renaming the server.
+	Function string `json:"function,omitempty"`
+
+	// Data contains the new server name.
+	Data RenameRequestData `json:"data,omitempty"`
+}
+
+// RenameRequestData holds the new server name to be set.
+type RenameRequestData struct {
+	// ServerName is the new name for the Satisfactory server.
+	ServerName string `json:"serverName,omitempty"`
+}
+
+// RenameServer renames the Satisfactory dedicated server to the specified serverName.
+func (c *GoFactoryClient) RenameServer(ctx context.Context, serverName string) error {
+	functionBody, err := json.Marshal(RenameRequest{
+		Function: RenameServerFunction,
+		Data: RenameRequestData{
+			ServerName: serverName,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	req, err := c.CreatePostRequest(RenameServerFunction, functionBody)
+	if err != nil {
+		return err
+	}
+
+	var apiError APIError
+	err = c.SendPostRequest(ctx, req, &apiError)
 	if err != nil {
 		return err
 	}
