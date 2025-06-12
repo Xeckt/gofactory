5d01a30a322f263b252574ecc27c6e0088522a9b
Node <jordansavell1198@gmail.com>
2025-06-12 14:06:33 +0100

    fix cli main.go stupid error text

--- cli/main.go
+++ cli/main.go
@@ -1,10 +1,12 @@
 package main
 
-import "github.com/alchemicalkube/gofactory/cli/cmd"
+import (
+	"github.com/alchemicalkube/gofactory/cli/cmd"
+)
 
 func main() {
 	err := cmd.Root.Execute()
 	if err != nil {
-		cmd.Logger.Trace("WTF")
+		cmd.Logger.Trace(err.Error())
 	}
 }
