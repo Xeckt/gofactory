7c02c97c6ea45f021f5731eefb6dfdac61ed891f
Node <jordansavell1198@gmail.com>
2025-06-12 15:24:44 +0100

    move healthcheck to individual functionm

--- cli/cmd/checkhealth.go
+++ cli/cmd/checkhealth.go
@@ -9,24 +9,27 @@ var healthCheckCmd = &cobra.Command{
 	Short: "Run basic health check against the HTTPS api",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		Logger.Trace("health check command",
-			Logger.Args(
-				"Context", ctx,
-				"Client pointer", &client),
-		)
-		server, err := client.GetServerHealth(ctx, "gofactory-cli-healthcheck-call")
-		if err != nil {
-			Logger.Fatal("healthcheck error", Logger.Args("error", err, "context", ctx, "object", server))
-		}
-		if server == nil {
-			Logger.Fatal("healthcheck command returned nil response")
-		}
-		if len(server.CustomData) > 0 {
-			Logger.Info("health check returned custom data", Logger.Args("Health", server,
-				"Custom Data", server.CustomData))
-		}
-		Logger.Info("health check returned successfully", Logger.Args("Health", server.Health))
+		healthCheck()
 	},
+}
+
+func healthCheck() {
+	Logger.Trace("health check command", Logger.Args(
+		"Context", ctx,
+		"Client pointer", &client),
+	)
+	server, err := client.GetServerHealth(ctx, "gofactory-cli-healthcheck-call")
+	if err != nil {
+		Logger.Fatal("healthcheck error", Logger.Args("error", err, "context", ctx, "object", server))
+	}
+	if server == nil {
+		Logger.Fatal("healthcheck command returned nil response")
+	}
+	if len(server.CustomData) > 0 {
+		Logger.Info("health check returned custom data", Logger.Args("Health", server,
+			"Custom Data", server.CustomData))
+	}
+	Logger.Info("health check returned successfully", Logger.Args("Health", server.Health))
 }
 
 func init() {
